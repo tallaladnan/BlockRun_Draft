c8351a477e43cde61a7ee38aa4f4d937f4185be4
tallaladnan <tallaladnan26@gmail.com>
2023-05-20 19:04:03 +0500

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEndded = false;
+    public float restartDelay = 2f;
+    public GameObject completeLevelUI;
+    
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEndded == false)
+        {
+            gameHasEndded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
